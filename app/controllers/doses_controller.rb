47104578c35a61529a6c823086b08236474b299b
WimJan Dannijs <wjdannijs@gmail.com>
2017-09-07 12:40:03 +0200

    adding controller for doses

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,10 @@
+class DosesController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def delete
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'doses/new'
+
+  get 'doses/create'
+
+  get 'doses/delete'
+
   get 'ingredients/index'
 
   get 'ingredients/show'
