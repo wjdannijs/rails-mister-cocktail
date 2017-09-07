6e6a2699d123e876c28acae440e97f32bff773d1
WimJan Dannijs <wjdannijs@gmail.com>
2017-09-07 12:36:18 +0200

    creating 3 models and 2 controllers

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,22 @@
+class CocktailsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def delete
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
+  get 'ingredients/index'
+
+  get 'ingredients/show'
+
+  get 'cocktails/index'
+
+  get 'cocktails/new'
+
+  get 'cocktails/create'
+
+  get 'cocktails/edit'
+
+  get 'cocktails/show'
+
+  get 'cocktails/update'
+
+  get 'cocktails/delete'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
