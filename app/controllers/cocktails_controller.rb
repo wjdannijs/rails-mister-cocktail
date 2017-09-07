93a4dc942c4e58fedfe04942cdf89c42847e84cc
WimJan Dannijs <wjdannijs@gmail.com>
2017-09-07 14:34:20 +0200

    finished cocktail controller and added basic cocktail routes

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -11,20 +11,30 @@ class CocktailsController < ApplicationController
 
   def create
     @cocktail = Cocktail.new(cocktail_params)
-    @cocktail.save
-    redirect_to cocktail_path(@cocktail)
-  end
-
-  def edit
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
   end
 
   def show
   end
 
+  def edit
+  end
+
   def update
+    if @cocktail.update(cocktail_params)
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :edit
+    end
   end
 
   def delete
+    @cocktail.destroy!
+    redirect_to cocktails_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,8 @@
 Rails.application.routes.draw do
-  get 'doses/new'
 
-  get 'doses/create'
+  root 'cocktails#index'
 
-  get 'doses/delete'
-
-  get 'ingredients/index'
-
-  get 'ingredients/show'
-
-  get 'cocktails/index'
-
-  get 'cocktails/new'
-
-  get 'cocktails/create'
-
-  get 'cocktails/edit'
-
-  get 'cocktails/show'
-
-  get 'cocktails/update'
-
-  get 'cocktails/delete'
+  resources :cocktails
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
